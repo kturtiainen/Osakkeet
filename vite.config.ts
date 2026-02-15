e3a7de6a5809a17608dfebaf74bf599c94f2bb4f
kturtiainen <40464894+kturtiainen@users.noreply.github.com>
2026-02-15 22:40:33 +0200

    Refactor Vite configuration for base path handling

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,8 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-//export default defineConfig({
-//  base: '/Osakkeet/',
- export default defineConfig(({ command }) => ({
-   base: command === 'build' ? '/Osakkeet/' : '/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/Osakkeet/' : '/',
   plugins: [
     react(),
     VitePWA({
@@ -55,4 +53,4 @@ import { VitePWA } from 'vite-plugin-pwa'
       }
     })
   ]
-})
+}))
